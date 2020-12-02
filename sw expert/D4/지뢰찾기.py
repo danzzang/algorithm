d0c0b63eff2fc87db3f62bc4e870c1d32af6bf1f
이다인 <ekdls0213@gmail.com>
2020-12-02 16:03:19 +0900

    12/2

--- sw expert/D4/지뢰찾기.py
+++ sw expert/D4/지뢰찾기.py
@@ -5,8 +5,8 @@ T = int(input())
 for test in range(1):
     N = int(input())
     board = [list(input()) for _ in range(N)]
-    num = 8
     
+
     for x in range(len(board)):
         print(board[x])
 
